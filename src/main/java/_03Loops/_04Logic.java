c19c9ae024259c8d5c53b32ae9059f5ac4507f38
atezgider <atezgider@gmail.com>
2021-01-06 20:56:09 +0100

    2021-01-06, 3

---
+++ src/main/java/_03Loops/_04Logic.java
@@ -0,0 +1,33 @@
+package _03Loops;
+
+public class _04Logic {
+
+    public static void main(String[] args) {
+
+        /*
+            &&  -> and      shortcut
+            ||  -> or       shortcut
+
+            &   -> bitwise and
+            |   -> bitwise or
+
+            && ve &  islemleri ve sonuclari ayni
+            || ve |  islemleri ve sonuclari ayni
+
+         */
+
+
+        int i = 10;
+        int j = 20;
+
+        if (i++ > 5 || j-- < 30){   // sart saglaniyorsa ikinci kondition okunmaz
+            System.out.println(i + " , " + j);
+        }
+
+        i = 10;
+        j = 20;
+        if (i++ > 5 | j-- < 30){            // her iki kondition okunur
+            System.out.println(i + " , " + j);
+        }
+    }
+}
